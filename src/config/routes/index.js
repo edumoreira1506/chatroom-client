788995edfe71c5cd3b5d8f37c1b73634916ee368
Eduardo Moreira <eduardo.moreira@codeminer42.com>
2020-02-01 20:21:33 -0300

    Add style to login page

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -11,6 +11,7 @@ import Login from '../../pages/Login';
 const Routes = () => (
   <BrowserRouter>
     <Switch>
+      <PublicRoute path="/" exact component={Login} />
       <PublicRoute path="/login" exact component={Login} />
 
       <PrivateRoute path="/chat" exact component={Login} />
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import './index.scss';
+
 const Login = () => (
-  <h1>
-    Login
-  </h1>
+  <section className="Login">
+    <div className="Login__title">
+      <h2 className="Login__title-text">
+        MultroomChat - Login
+      </h2>
+    </div>
+    <div className="Login__form-area">
+      <form className="Login__form">
+        <input className="Login__input" type="text" placeholder="Nome aleatório ai caraio" required />
+        <input className="Login__input" type="submit" value="Submit" required />
+      </form>
+    </div>
+  </section>
 );
 
 export default Login;
