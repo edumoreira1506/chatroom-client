26b990f508bf61e04fd46d4ddf9d79455a894ae3
edumoreira1506 <contato@eduardoem.com.br>
2020-02-03 14:07:25 -0300

    Updates

---
+++ precache-manifest.d6bf3854de7874b9f0ec46ab31432007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a7eb5209c6286dcef9d3ff7ce635af",
+    "url": "/chatroom-client/index.html"
+  },
+  {
+    "revision": "430c7d2bfedda57443a8",
+    "url": "/chatroom-client/static/css/main.69c5b103.chunk.css"
+  },
+  {
+    "revision": "dd70a4ca0ad410a3ca35",
+    "url": "/chatroom-client/static/js/2.b47750fb.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/chatroom-client/static/js/2.b47750fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430c7d2bfedda57443a8",
+    "url": "/chatroom-client/static/js/main.257ce824.chunk.js"
+  },
+  {
+    "revision": "f57b2bd16efcc1427253",
+    "url": "/chatroom-client/static/js/runtime-main.9f287769.js"
+  }
+]);
